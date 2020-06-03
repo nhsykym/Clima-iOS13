793967c874f64b83268dba5720bdbadd201d0b2b
Naohisa Yokoyama <nhsykym@gmail.com>
2020-06-03 15:55:50 +0900

    Implement weather app using OpenWeatherAPI and CoreLocation.

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  weatherData.swift
+//  Clima
+//
+//  Created by 横山尚久 on 2020/06/03.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
